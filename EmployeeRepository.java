97c0018b14781a84b79118988b3361a0c4441607
Dinesh Malik <admin@admins-mbp.mynet>
2020-04-02 23:10:56 +0100

    second commit

---
+++ EmployeeRepository.java
@@ -0,0 +1,11 @@
+package com.example.db.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.db.entity.Employee;
+
+@Repository
+public interface EmployeeRepository  extends JpaRepository<Employee, Integer> {
+
+}
